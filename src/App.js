7b52cd26f327ecfff91064eda312dd463d4df6a2
bzerka <igorcastro55@hotmail.com>
2022-06-15 17:08:12 -0300

    igor-juncao

--- src/App.js
+++ src/App.js
@@ -64,7 +64,7 @@ class App extends React.Component{
           </Produtos>
         </ContainerProdutos>
         <ContainerCarrinho>
-          <Carrinho />
+          
         </ContainerCarrinho>
       </section>
       <footer>Footer</footer>
