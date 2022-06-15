2caf6d7988f74f8be31182ac5938ec76b7e20529
bzerka <igorcastro55@hotmail.com>
2022-06-15 14:55:41 -0300

    igor-atualizacao

---
+++ src/Components/Carrinho/Carrinho.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import styled from 'styled-components';
+
+class Carrinho extends React.Component {
+
+    state = {
+      produtos: []
+    }
+    
+    render() {
+      return(
+        <div></div>
+  )}
+}
+
+export default Carrinho
+
