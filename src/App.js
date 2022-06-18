f1a4465bf651ad78252532d6855a150929267e84
Joclelson <jocleelson.159@hotmail.com>
2022-06-18 13:49:07 -0300

    Estilização do site

--- src/App.js
+++ src/App.js
@@ -1,50 +1,46 @@
 import React from "react";
 import "./App.css";
 import styled from "styled-components";
-import Produto from './Components/Produto/Produto';
-import Filtro from './Components/Filtro/Filtro';
+import Produto from "./Components/Produto/Produto";
+import Filtro from "./Components/Filtro/Filtro";
 import Carrinho from "./Components/Carrinho/Carrinho";
 
 const Produtos = styled.div`
   display: flex;
-  border: 1px solid black;
   width: 60%;
-`
+`;
 
-class App extends React.Component{
-
+class App extends React.Component {
   state = {
-    produto: [{
-      id: 1,
-      imagem: "https://picsum.photos/200/300?random=1",
-      nome: "Produto",
-      preco: 100,
-      quantidade: 1
-
-    },
-    {
-      id: 2,
-      imagem: "https://picsum.photos/200/300?random=2",
-      nome: "Aroduto",
-      preco: 350,
-      quantidade: 1
-
-    },
-    {
-      id: 3,
-      imagem: "https://picsum.photos/200/300?random=3",
-      nome: "Zroduto",
-      preco: 50,
-      quantidade: 1
-    },
-    {
-      id: 4,
-      imagem: "https://picsum.photos/200/300?random=4",
-      nome: "Produto",
-      preco: 500,
-      quantidade: 1
-
-    }
+    produto: [
+      {
+        id: 1,
+        imagem: "https://picsum.photos/200/300?random=1",
+        nome: "Produto",
+        preco: 100,
+        quantidade: 1,
+      },
+      {
+        id: 2,
+        imagem: "https://picsum.photos/200/300?random=2",
+        nome: "Aroduto",
+        preco: 350,
+        quantidade: 1,
+      },
+      {
+        id: 3,
+        imagem: "https://picsum.photos/200/300?random=3",
+        nome: "Zroduto",
+        preco: 50,
+        quantidade: 1,
+      },
+      {
+        id: 4,
+        imagem: "https://picsum.photos/200/300?random=4",
+        nome: "Produto",
+        preco: 500,
+        quantidade: 1,
+      },
     ],
     carrinho: [],
     inputValorMin: "",
@@ -52,13 +48,13 @@ class App extends React.Component{
     inputBuscar: "",
     precoTotal: 0,
     filtro: "crescente",
-  }
+  };
 
   limparFiltro = () => {
-    this.setState({inputValorMin: ""})
-    this.setState({inputValorMax: ""})
-    this.setState({inputBuscar: ""})
-  }
+    this.setState({ inputValorMin: "" });
+    this.setState({ inputValorMax: "" });
+    this.setState({ inputBuscar: "" });
+  };
 
   adicionarItemCarrinho = (produto) => {
     const novoItem = {
@@ -66,76 +62,98 @@ class App extends React.Component{
       nome: produto.nome,
       preco: produto.preco,
       quantidade: 1,
-    }
+    };
 
-    let novaListaCarrinho = [...this.state.carrinho, novoItem]
+    let novaListaCarrinho = [...this.state.carrinho, novoItem];
 
     novaListaCarrinho = novaListaCarrinho.filter(function (produto) {
-      return !this[JSON.stringify(produto)] && (this[JSON.stringify(produto)] = true);
-    }, Object.create(null))
-    
-    this.setState({carrinho: novaListaCarrinho})
-  } 
+      return (
+        !this[JSON.stringify(produto)] && (this[JSON.stringify(produto)] = true)
+      );
+    }, Object.create(null));
+
+    this.setState({ carrinho: novaListaCarrinho });
+  };
 
   adicionarQuantidade = (index) => {
-    
     const novoItem = [...this.state.carrinho];
-    if(novoItem[index].quantidade >= 0) {
-    novoItem[index].quantidade++;	
+    if (novoItem[index].quantidade >= 0) {
+      novoItem[index].quantidade++;
     }
-    this.setState({carrinho: novoItem})
+    this.setState({ carrinho: novoItem });
   };
 
   removerQuantidade = (index) => {
     const novoItem = [...this.state.carrinho];
-    if(novoItem[index].quantidade > 1) {
-    novoItem[index].quantidade--;	
+    if (novoItem[index].quantidade > 1) {
+      novoItem[index].quantidade--;
     }
-    this.setState({carrinho: novoItem})
+    this.setState({ carrinho: novoItem });
   };
 
   removeProduto = (id) => {
     const produtoRemovido = this.state.carrinho.filter((produto) => {
-      return id !== produto.id
+      return id !== produto.id;
     });
-    this.setState({carrinho: produtoRemovido})
-  }
+    this.setState({ carrinho: produtoRemovido });
+  };
 
   onChangeInputValorMin = (event) => {
-    this.setState({inputValorMin: event.target.value})
-  }
+    this.setState({ inputValorMin: event.target.value });
+  };
 
   onChangeInputValorMax = (event) => {
-    this.setState({inputValorMax: event.target.value})
-  }
+    this.setState({ inputValorMax: event.target.value });
+  };
 
   onChangeInputBuscar = (event) => {
-    this.setState({inputBuscar: event.target.value})
-  }
+    this.setState({ inputBuscar: event.target.value });
+  };
 
   onChangeFilter = (event) => {
-    this.setState({filtro: event.target.value})
-  }
+    this.setState({ filtro: event.target.value });
+  };
 
   render() {
-
-
-  return (
-    <div className="container">
-      <header>header</header>
-      <section className="main-container">
-        <Filtro inputValorMin={this.state.inputValorMin} onChangeInputValorMin={this.onChangeInputValorMin}
-        inputValorMax={this.state.inputValorMax} onChangeInputValorMax={this.onChangeInputValorMax}
-        inputBuscar={this.state.inputBuscar} onChangeInputBuscar={this.onChangeInputBuscar} limparFiltro={this.limparFiltro}/>
+    return (
+      <div className="container">
+        <header>
+          <h2>Mundo do <span>Astronauta</span></h2>
+          <button>Minha Conta</button>
+        </header>
+        <section className="main-container">
+          <Filtro
+            inputValorMin={this.state.inputValorMin}
+            onChangeInputValorMin={this.onChangeInputValorMin}
+            inputValorMax={this.state.inputValorMax}
+            onChangeInputValorMax={this.onChangeInputValorMax}
+            inputBuscar={this.state.inputBuscar}
+            onChangeInputBuscar={this.onChangeInputBuscar}
+            limparFiltro={this.limparFiltro}
+          />
           <Produtos>
-            <Produto filtro={this.state.filtro} onChangeFilter={this.onChangeFilter} adicionarItemCarrinho={this.adicionarItemCarrinho} produto={this.state.produto} inputValorMin={this.state.inputValorMin} inputValorMax={this.state.inputValorMax} inputBuscar={this.state.inputBuscar}/>
+            <Produto
+              filtro={this.state.filtro}
+              onChangeFilter={this.onChangeFilter}
+              adicionarItemCarrinho={this.adicionarItemCarrinho}
+              produto={this.state.produto}
+              inputValorMin={this.state.inputValorMin}
+              inputValorMax={this.state.inputValorMax}
+              inputBuscar={this.state.inputBuscar}
+            />
           </Produtos>
-          <Carrinho removerQuantidade={this.removerQuantidade} adicionarQuantidade={this.adicionarQuantidade} precoTotal={this.state.precoTotal} removeProduto={this.removeProduto} carrinho={this.state.carrinho}/>
-      </section>
-      <footer>Footer</footer>
-    </div>
-  );
-}
+          <Carrinho
+            removerQuantidade={this.removerQuantidade}
+            adicionarQuantidade={this.adicionarQuantidade}
+            precoTotal={this.state.precoTotal}
+            removeProduto={this.removeProduto}
+            carrinho={this.state.carrinho}
+          />
+        </section>
+        <footer>Footer</footer>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/Components/Produto/Produto.js
+++ src/Components/Produto/Produto.js
@@ -1,11 +1,11 @@
 import React from "react";
 import styled from "styled-components";
 
-
 const Card = styled.div`
   height: 320px;
   width: 202px;
-  border: 1px solid black;
+  border: 2px solid blueviolet;
+  border-radius: 6px;
   display: flex;
   flex-direction: column;
   align-items: center;
@@ -26,6 +26,15 @@ const ContainerTexto = styled.div`
   gap: 8px;
 `;
 
+const Botao = styled.button `
+  padding: 7px 6px;
+  background-color: blueviolet;
+  color: #fff;
+  border: none;
+  border-radius: 6px;
+  cursor: pointer;
+`
+
 const ContainerCard = styled.div`
   display: flex;
   flex-wrap: wrap;
@@ -35,69 +44,80 @@ const HeaderCards = styled.div`
   display: flex;
   width: 100%;
   justify-content: space-between;
-`
+`;
 
 const MainContainer = styled.div`
   width: 100%;
-`
+`;
 
 class Produto extends React.Component {
-
   render() {
-    
-      let cardsFiltrados
+    let cardsFiltrados;
 
-      if(this.props.inputValorMin >= 0 && this.props.inputValorMax >= 0) {
-        cardsFiltrados = this.props.produto
-        .filter(produto => {
-          return produto.nome.toLowerCase().includes(this.props.inputBuscar.toLowerCase())
+    if (this.props.inputValorMin >= 0 && this.props.inputValorMax >= 0) {
+      cardsFiltrados = this.props.produto
+        .filter((produto) => {
+          return produto.nome
+            .toLowerCase()
+            .includes(this.props.inputBuscar.toLowerCase());
         })
-        .filter(produto => {
-          return this.props.inputValorMin === "" || produto.preco >= this.props.inputValorMin
+        .filter((produto) => {
+          return (
+            this.props.inputValorMin === "" ||
+            produto.preco >= this.props.inputValorMin
+          );
         })
-        .filter(produto => {
-          return this.props.inputValorMax === "" || produto.preco <= this.props.inputValorMax
+        .filter((produto) => {
+          return (
+            this.props.inputValorMax === "" ||
+            produto.preco <= this.props.inputValorMax
+          );
         })
         .sort((produto, segundoProduto) => {
-          switch (this.props.filtro){
+          switch (this.props.filtro) {
             case "crescente":
-              return produto.preco - segundoProduto.preco
+              return produto.preco - segundoProduto.preco;
             default:
-              return segundoProduto.preco - produto.preco
-
+              return segundoProduto.preco - produto.preco;
           }
         })
         .map((produto, index) => {
-          return (  
+          return (
             <Card key={index}>
               <Img src={produto.imagem} />
               <ContainerTexto>
                 <p>{produto.nome}</p>
                 <p>R$:{produto.preco}</p>
-                <button onClick={() => this.props.adicionarItemCarrinho(produto)}>Adicionar ao carrinho</button>
+                <Botao
+                  onClick={() => this.props.adicionarItemCarrinho(produto)}
+                >
+                  Adicionar ao carrinho
+                </Botao>
               </ContainerTexto>
             </Card>
-          )
-        })
-      }
+          );
+        });
+    }
 
     return (
       <MainContainer>
         <HeaderCards>
           <div>
-          <p>Quantidade de produtos: {this.props.produto.length}</p>
+            <p>Quantidade de produtos: {this.props.produto.length}</p>
           </div>
           <div>
             <label for="filtro">Ordenação: </label>
-            <select name="filtro" value={this.props.filtro} onChange={this.props.onChangeFilter}>
+            <select
+              name="filtro"
+              value={this.props.filtro}
+              onChange={this.props.onChangeFilter}
+            >
               <option value="crescente">Crescente</option>
               <option value="decrescente">Decrescente</option>
             </select>
           </div>
         </HeaderCards>
-        <ContainerCard>
-          {cardsFiltrados}
-        </ContainerCard>
+        <ContainerCard>{cardsFiltrados}</ContainerCard>
       </MainContainer>
     );
   }
